9c83dd241c9bd6c509d33c8fe30ed337ec6a00ff
carusocr <carusochrisr@gmail.com>
2016-06-01 17:53:30 -0700

    db connectivity

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var alexa = require('alexa-app');
 var mysql = require('mysql');
 var iniparser = require('iniparser')
 var config = iniparser.parseSync('db.ini')
-console.log(config.user);
 //var env = process.env.NODE_ENV || 'dev'; //startup nodejs with e.g:  NODE_ENV= node server.js
 
 con = mysql.createConnection({
@@ -20,15 +19,23 @@ con.connect(function(err) {
 	console.log("Connection established.");
 });
 
-con.query('select mood from moodchecker',function (err, rows){
+// need to wrap this in a function that gets called 
+// how to populate list of possible items??
+con.query('select * from grocery_list',function (err, rows){
 	if(err) throw err;
 
-	console.log(rows[0].mood);
+	for (var i in rows) {
+		console.log("Item:", rows[i].item, "\nPrice:", rows[i].price);
+	}
+	var sardines = rows[i].item;
+	var sardine_price = rows[i].price;
+	var store = rows[i].store;
 });
 
 // Define an alexa-app
-var app = new alexa.app('moodchecker');
-
+var app = new alexa.app('alexa_shopper');
+var sardines = 'sardines';
+var store = 'Q F C'
 app.launch(function(req,res) {
   response.session ('open_session', 'true');
     response.say("Welcome to mood checker. I want to know how you're doing.");
@@ -43,56 +50,20 @@ app.intent('HelpIntent',
     ]
   },
   function (req,res) {
-    res.say("Rate how you're feeling by saying a number between one and ten. That's all I do for now.");
+    //res.say("Rate how you're feeling by saying a number between one and ten. That's all I do for now.");
+  	res.say("Hey, " + sardines + " are on sale right now at " + store);
   }
 );
 
-app.intent('FeelsIntent',
-  {
-    "slots": {"rating":"NUMBER"},
-    "utterances": [
-      "{i feel|i am|maybe|around|i'm|about|} {a|around|about|} {a|} {rating}"]
-  },
-  function(req,res) {
-    var feel_rating = req.slot('rating');
-    res.say("You said that you're feeling around a" + feel_rating + "on a scale of one to ten. Why is that?");
-  }
-);
-
-app.intent('guess',{
-		"slots":{"guess":"NUMBER"}
-		,"utterances":["{1-100|guess}"]
-	},
-	function(req,res) {
-		var guesses = (+req.session('guesses'))+1;
-		var guess = req.slot('guess');
-		var number = +req.session('number');
-		if (!guess) {
-			res.say("Sorry, I didn't hear a number. The number was "+number);
-		}
-		else if (guess==number) {
-			res.say("Congratulations, you guessed the number in " + guesses + (guesses==1?" try":" tries"));
-		}
-		else {
-			if (guess > number) {
-				res.say("Guess lower");
-			}
-			else if (guess < number) {
-				res.say("Guess higher");
-			}
-			res.reprompt("Sorry, I didn't hear a number. Try again.");
-			res.session('guesses',guesses);
-			res.shouldEndSession(false);
-		}
-	}
-);
 exports.handler = app.lambda();
 
 if ((process.argv.length === 3) && (process.argv[2] === 'schema')){
   console.log (app.schema ());
   console.log (app.utterances ());
 }
-console.log (app.schema());
+//console.log (app.schema());
+//console.log (app.utterances ());
+
 
 con.end(function(err) {
 	//Ends gracefully
