d79edf14a7a247b69d7274bde9c1120609488303
Chris Caruso <carusochrisr@gmail.com>
2016-06-07 19:55:00 -0700

    moved query to shop intent

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/*
+TO-DO: how to auto-add database grocery items to slots?
+*/
 var alexa = require('alexa-app');
 var mysql = require('mysql');
 var iniparser = require('iniparser')
 var config = iniparser.parseSync('db.ini')
-//var env = process.env.NODE_ENV || 'dev'; //startup nodejs with e.g:  NODE_ENV= node server.js
 
 con = mysql.createConnection({
     host        : config.host,
@@ -17,38 +19,10 @@ con.connect(function(err) {
 		return;
 	}
 	console.log("Connection established.");
+
 });
 
-// need to wrap this in a function that gets called 
-// how to populate list of possible items??
-
-var buildList = function() {
-  con.query('select * from grocery_list',function (err, rows){
-  	if(err) callback(err);
-
-  	for (var i in rows) {
-  		console.log("Item:", rows[i].item, "\nPrice:", rows[i].price);
-  	}
-  	var sardines = rows[0].item;
-  	var sardine_price = rows[0].price;
-  	var store = rows[0].store;
-    console.log(rows[0].item);
-  });
-}
-
-var testList = function(callback) {
-  callback('sardines');
-}
-
-//testList(buildList);
-
-//rows = buildList();
-// Define an alexa-app
 var app = new alexa.app('shopper');
-//var sardines = 'sardines';
-//var store = 'Q F C'
-var sardines='sardines';
-var store='zug';
 
 app.launch(function(request,response) {
   response.session ('open_session', 'true');
@@ -59,22 +33,11 @@ app.launch(function(request,response) {
 app.intent('shopIntent',
   {
     "slots" :  {"foodItem": "GroceryItem"},
-    "utterances": [ "{who is selling|where's|where is|I'm looking for} {the cheapest|a|} {-|foodItem}"]
+    "utterances": [ "{find me|who is selling|where's|where is|I'm looking for} {the cheapest|a|} {-|foodItem}"]
   },
   function(req,res) {
     var food_target = req.slot('foodItem');
-    res.say("You said that you're looking for " + food_target);
-  }
-);
-
-app.intent('HelpIntent',
-  {
-    "slots" : {},
-    "utterances": [
-      "help"
-    ]
-  },
-  function (req,res) {
+    res.say("You said that you're looking for " + food_target + "...");
     con = mysql.createConnection({
       host        : config.host,
       user        : config.user,
@@ -89,90 +52,41 @@ app.intent('HelpIntent',
       }
       console.log("Connection established.");
     });
-    con.query('select * from grocery_list',function (err, rows){
+    var sql = 'select * from grocery_list where item like "%' + food_target + '%"';
+    con.query(sql, function (err, rows){
       if(err) callback(err);
 
       for (var i in rows) {
         console.log("Item:", rows[i].item, "\nPrice:", rows[i].price);
       }
-      var sardines = rows[0].item;
+      var food = rows[0].item; //food_target already has name, use that?
       var price = rows[0].price;
       var store = rows[0].store;
-      res.say("I found " + sardines + " for " + price + " at " + store);
+      res.say("I found " + food_target + " for " + price + " at " + store);
       res.send();
     });
     return false;
-    //res.say("Say the name of a grocery item that you're looking for and I'll seek the lowest price. Not guaranteed, dumbshit!");
-  	//res.say("Hey, " + sardines + " are on sale right now at " + store);
   }
 );
 
-/*
-
-TO-DO: move db query to findItem function, and add grocery items to slots?
-
-app.intent('FeelsIntent',
+app.intent('HelpIntent',
   {
-    "slots": {"rating":"NUMBER"},
+    "slots" : {},
     "utterances": [
-      "{i feel|i am|maybe|around|i'm|about|} {a|around|about|} {a|} {rating}"]
+      "help"
+    ]
   },
-  function(req,res) {
-    var feel_rating = req.slot('rating');
-    res.say("You said that you're feeling around a" + feel_rating + "on a scale of one to ten. Why is that?");
-  }
-);
-*/
-/*
-app.intent('FeelsIntent',
-  {
-    "slots": {"rating":"NUMBER"},
-    "utterances": [
-      "{i feel|i am|maybe|around|i'm|about|} {a|around|about|} {a|} {rating}"]
-  },
-  function(req,res) {
-    var feel_rating = req.slot('rating');
-    res.say("You said that you're feeling around a" + feel_rating + "on a scale of one to ten. Why is that?");
+  function(req,res){
+    res.say("Say the name of a grocery item that you're looking for and I'll seek the lowest price. Not guaranteed, dumbshit!");
   }
 );
 
-app.intent('guess',{
-    "slots":{"guess":"NUMBER"}
-    ,"utterances":["{1-100|guess}"]
-  },
-  function(req,res) {
-    var guesses = (+req.session('guesses'))+1;
-    var guess = req.slot('guess');
-    var number = +req.session('number');
-    if (!guess) {
-      res.say("Sorry, I didn't hear a number. The number was "+number);
-    }
-    else if (guess==number) {
-      res.say("Congratulations, you guessed the number in " + guesses + (guesses==1?" try":" tries"));
-    }
-    else {
-      if (guess > number) {
-        res.say("Guess lower");
-      }
-      else if (guess < number) {
-        res.say("Guess higher");
-      }
-      res.reprompt("Sorry, I didn't hear a number. Try again.");
-      res.session('guesses',guesses);
-      res.shouldEndSession(false);
-    }
-  }
-);
-*/
 exports.handler = app.lambda();
 
 if ((process.argv.length === 3) && (process.argv[2] === 'schema')){
   console.log (app.schema ());
   console.log (app.utterances ());
 }
-//console.log (app.schema());
-//console.log (app.utterances ());
-
 
 con.end(function(err) {
 	//Ends gracefully
